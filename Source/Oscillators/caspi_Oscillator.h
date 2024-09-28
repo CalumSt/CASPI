07f4e2191f55d310db85c279dfa89a8783b69905
CalumSt <calum.stuart@protonmail.com>
2024-09-28 12:56:25 +0100

    Added googletest as dependency, added initial BLEP oscillator

--- Source/Oscillators/caspi_Oscillator.h
+++ Source/Oscillators/caspi_Oscillator.h
@@ -1,22 +1,16 @@
-//
-// Created by CS Islay on 22/09/2024.
-//
-
-#ifndef CASPI_OSCILLATOR_H
-#define CASPI_OSCILLATOR_H
-
 /************************************************************************
-      ___           ___           ___           ___
-     /\__\         /\  \         /\__\         /\  \
-    /:/  /        /::\  \       /:/ _/_       /::\  \     ___
-   /:/  /        /:/\:\  \     /:/ /\  \     /:/\:\__\   /\__\
-  /:/  /  ___   /:/ /::\  \   /:/ /::\  \   /:/ /:/  /  /:/__/
- /:/__/  /\__\ /:/_/:/\:\__\ /:/_/:/\:\__\ /:/_/:/  /  /::\  \
- \:\  \ /:/  / \:\/:/  \/__/ \:\/:/ /:/  / \:\/:/  /   \/\:\  \__
-  \:\  /:/  /   \::/__/       \::/ /:/  /   \::/__/     ~~\:\/\__\
-   \:\/:/  /     \:\  \        \/_/:/  /     \:\  \        \::/  /
-    \::/  /       \:\__\         /:/  /       \:\__\       /:/  /
-     \/__/         \/__/         \/__/         \/__/       \/__/
+
+ .d8888b.                             d8b
+d88P  Y88b                            Y8P
+888    888
+888         8888b.  .d8888b  88888b.  888
+888            "88b 88K      888 "88b 888
+888    888 .d888888 "Y8888b. 888  888 888
+Y88b  d88P 888  888      X88 888 d88P 888
+ "Y8888P"  "Y888888  88888P' 88888P"  888
+                             888
+                             888
+                             888
 
 
 * @file caspi_Oscillator.h
@@ -26,8 +20,17 @@
 *
 ************************************************************************/
 
-class caspi_Oscillator {
 
+#ifndef CASPI_OSCILLATOR_H
+#define CASPI_OSCILLATOR_H
+
+template <typename FloatType>
+class caspi_Oscillator {
+public:
+    virtual ~caspi_Oscillator() = default;
+    virtual FloatType getNextSample() = 0;
+    virtual void setFrequency() = 0;
+    virtual void reset() = 0;
 };
 
 #endif //CASPI_OSCILLATOR_H
