a9cd6abe506c276175b7836d7041a81bdfad7d7c
CalumSt <calum.stuart@protonmail.com>
2024-11-30 17:18:24 +0000

    Added Matplot and initial FFT

--- Source/Utilities/caspi_Assert.h
+++ Source/Utilities/caspi_Assert.h
@@ -6,4 +6,9 @@
 #define CASPI_ASSERT(x,msg) assert(x && msg)
 #endif
 
+#ifndef CASPI_STATIC_ASSERT
+#include <cassert>
+#define CASPI_STATIC_ASSERT(x,msg) static_assert(x && msg)
+#endif
+
 #endif //CASPI_ASSERT_H
--- Tests/Oscillator_test.cpp
+++ Tests/Oscillator_test.cpp
@@ -2,17 +2,55 @@
 #define OSCILLATOR_TEST_H
 
 #include <gtest/gtest.h>
+
+#include <utility>
 #include "Oscillators/caspi_BlepOscillator.h"
 #include "Oscillators/caspi_PMOperator.h"
+#include "test_helpers.h"
 
 // Test params
 constexpr auto frequency = 1000.0f;
 constexpr auto sampleRate = 44100.0f;
 constexpr auto renderTime = 1;
+constexpr auto modIndex   = 0.8f;
 
 // derived
 constexpr auto samplesToRender = renderTime * static_cast<int>(sampleRate);
 constexpr auto phaseIncrement = frequency / sampleRate;
+
+// Helper
+template <typename OscType>
+void generateWaveformGraph(OscType& osc, float frequency, float sampleRate,float numberOfWavelengths,std::string filename)
+{
+    const auto renderTimeForSingleWaveform = numberOfWavelengths * 1.0f / frequency;
+    const int numberOfSamples = static_cast<int>(renderTimeForSingleWaveform * sampleRate);
+    osc.resetPhase();
+    osc.setFrequency (frequency,sampleRate);
+    std::vector<float> times = std::vector<float>(numberOfSamples);
+    std::iota(std::begin(times), std::end(times), 0.0f);
+    std::vector<float> samples = std::vector<float>(numberOfSamples);
+    for (int i = 0;i < numberOfSamples; i++) {
+        samples.at(i) = osc.getNextSample();
+    }
+    createPlot<float> (times, samples, "Single Waveform", std::move(filename));
+}
+
+// Helper
+template <typename OscType>
+void generatePulseModulationWaveformGraph(OscType& osc, float frequency,float modIndex, float sampleRate,float numberOfWavelengths,std::string filename)
+{
+    const auto renderTimeForSingleWaveform = numberOfWavelengths * 1.0f / frequency;
+    const int numberOfSamples = static_cast<int>(renderTimeForSingleWaveform * sampleRate);
+    osc.reset();
+    osc.setFrequency (frequency,modIndex,sampleRate);
+    std::vector<float> times = std::vector<float>(numberOfSamples);
+    std::iota(std::begin(times), std::end(times), 0.0f);
+    std::vector<float> samples = std::vector<float>(numberOfSamples);
+    for (int i = 0;i < numberOfSamples; i++) {
+        samples.at(i) = osc.getNextSample();
+    }
+    createPlot<float> (times, samples, "Single Waveform", std::move(filename));
+}
 
 TEST(OscillatorTests, test) {
     EXPECT_TRUE(true);
@@ -73,6 +111,8 @@ TEST(OscillatorTests, SineRenderWaveform_test) {
         EXPECT_GE(s, -1.0f);
         EXPECT_LE(s, 1.0f);
     }
+
+    generateWaveformGraph (osc, frequency, sampleRate,3.0f, "sine_waveform");
 }
 
 TEST(OscillatorTests, SawRenderWaveform_test) {
@@ -87,6 +127,9 @@ TEST(OscillatorTests, SawRenderWaveform_test) {
         EXPECT_LE(s, 1.0f);
         currentPhase += phaseIncrement;
     }
+
+    generateWaveformGraph (osc, frequency, sampleRate,3.0f, "saw_waveform");
+
 }
 
 TEST(OscillatorTests, SquareRenderWaveform_test) {
@@ -101,6 +144,8 @@ TEST(OscillatorTests, SquareRenderWaveform_test) {
         EXPECT_LE(s, 1.0f);
         currentPhase += phaseIncrement;
     }
+
+    generateWaveformGraph (osc, frequency, sampleRate,3.0f, "square_waveform");
 }
 
 TEST(OscillatorTests, TriangleRenderWaveform_test) {
@@ -115,6 +160,25 @@ TEST(OscillatorTests, TriangleRenderWaveform_test) {
         EXPECT_LE(s, 1.0f);
         currentPhase += phaseIncrement;
     }
+
+    generateWaveformGraph (osc, frequency, sampleRate,3.0f, "triangle_waveform");
+}
+
+
+TEST(OscillatorTests, renderToBlock_test) {
+    CASPI::BlepOscillator::Sine<float> osc;
+    osc.setFrequency(frequency,sampleRate);
+    auto test = std::vector<float>(1024);
+    for (int i = 0;i < 1024; i++) {
+        auto s = osc.getNextSample();
+        test.push_back (s);
+    }
+    osc.resetPhase();
+    // This API is ugly but will work for now
+    auto output = CASPI::BlepOscillator::renderBlock<CASPI::BlepOscillator::Sine<float>,float>(frequency,sampleRate,1024);
+    for (int i = 0;i < 1024; i++) {
+        EXPECT_EQ(output[i],test[i]);
+    }
 }
 
 //======================================================================================
@@ -122,12 +186,14 @@ TEST(OscillatorTests, TriangleRenderWaveform_test) {
 
 TEST(OscillatorTests, PMOperator_test) {
     CASPI::PMOperator<float> osc;
-    osc.setFrequency(frequency,0.5f,sampleRate);
+    osc.setFrequency(frequency,modIndex,sampleRate);
     for (int i = 0;i < samplesToRender; i++) {
         auto s = osc.getNextSample();
         EXPECT_GE(s, -1.0f);
         EXPECT_LE(s, 1.0f);
     }
+
+    generatePulseModulationWaveformGraph (osc, frequency,modIndex, sampleRate,3.0f, "pm_operator_waveform");
 }
 
 
