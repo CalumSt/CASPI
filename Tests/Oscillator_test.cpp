7f6bce3c7b71f65aaf924e03bb838b07b463ab8c
CalumSt <calum.stuart@protonmail.com>
2024-12-18 12:52:51 +0000

    Finished simple PMOperator, separated out utility tests. Added PlotSignal.py for generating figures.

--- Tests/Oscillator_test.cpp
+++ Tests/Oscillator_test.cpp
@@ -3,85 +3,90 @@
 
 #include <gtest/gtest.h>
 
-#include <utility>
 #include "Oscillators/caspi_BlepOscillator.h"
 #include "Oscillators/caspi_PMOperator.h"
-#include "Oscillators/caspi_PMOperator_new.h"
 #include "test_helpers.h"
+#include <utility>
 
 // Test params
-constexpr auto frequency = 1000.0f;
+constexpr auto frequency  = 1000.0f;
 constexpr auto sampleRate = 44100.0f;
 constexpr auto renderTime = 1;
 constexpr auto modIndex   = 0.8f;
 
 // derived
-constexpr auto samplesToRender = renderTime * static_cast<int>(sampleRate);
-constexpr auto phaseIncrement = frequency / sampleRate;
+constexpr auto samplesToRender = renderTime * static_cast<int> (sampleRate);
+constexpr auto phaseIncrement  = frequency / sampleRate;
 
 // Helper
 template <typename OscType>
-void generateWaveformGraph(OscType& osc, float inputFrequency, float inputSampleRate, const float numberOfWavelengths,std::string filename)
+void generateWaveformGraph (OscType& osc, float inputFrequency, float inputSampleRate, const float numberOfWavelengths, std::string filename)
 {
     const auto renderTimeForSingleWaveform = numberOfWavelengths * 1.0f / inputFrequency;
-    const auto numberOfSamples = static_cast<int>(renderTimeForSingleWaveform * inputSampleRate);
+    const auto numberOfSamples             = static_cast<int> (renderTimeForSingleWaveform * inputSampleRate);
     osc.resetPhase();
-    osc.setFrequency (inputFrequency,inputSampleRate);
-    auto times = std::vector<float>(numberOfSamples);
-    std::iota(std::begin(times), std::end(times), 0.0f);
-    auto samples = std::vector<float>(numberOfSamples);
-    for (int i = 0;i < numberOfSamples; i++) {
-        samples.at(i) = osc.getNextSample();
+    osc.setFrequency (inputFrequency, inputSampleRate);
+    auto times = std::vector<float> (numberOfSamples);
+    std::iota (std::begin (times), std::end (times), 0.0f);
+    auto samples = std::vector<float> (numberOfSamples);
+    for (int i = 0; i < numberOfSamples; i++)
+    {
+        samples.at (i) = osc.getNextSample();
     }
-    createPlot<float> (times, samples, "Single Waveform", std::move(filename));
+    createPlot<float> (times, samples, "Single Waveform", std::move (filename));
 }
 
 // Helper
 template <typename OscType>
-void generatePulseModulationWaveformGraph(OscType& osc, float inputFrequency,float inputModIndex, float inputSampleRate, const float numberOfWavelengths,std::string filename)
+void generatePulseModulationWaveformGraph (OscType& osc, float inputFrequency, float inputModIndex, float inputSampleRate, const float numberOfWavelengths, std::string filename)
 {
     const auto renderTimeForSingleWaveform = numberOfWavelengths * 1.0f / inputFrequency;
-    const auto numberOfSamples = static_cast<int>(renderTimeForSingleWaveform * inputSampleRate);
+    const auto numberOfSamples             = static_cast<int> (renderTimeForSingleWaveform * inputSampleRate);
     osc.reset();
-    osc.setFrequency (inputFrequency,inputModIndex,inputSampleRate);
-    auto times = std::vector<float>(numberOfSamples);
-    std::iota(std::begin(times), std::end(times), 0.0f);
-    auto samples = std::vector<float>(numberOfSamples);
-    for (int i = 0;i < numberOfSamples; i++) {
-        samples.at(i) = osc.getNextSample();
+    osc.setFrequency (inputFrequency, inputModIndex, inputSampleRate);
+    auto times = std::vector<float> (numberOfSamples);
+    std::iota (std::begin (times), std::end (times), 0.0f);
+    auto samples = std::vector<float> (numberOfSamples);
+    for (int i = 0; i < numberOfSamples; i++)
+    {
+        samples.at (i) = osc.getNextSample();
     }
-    createPlot<float> (times, samples, "Single Waveform", std::move(filename));
+    createPlot<float> (times, samples, "Single Waveform", std::move (filename));
 }
 
-TEST(OscillatorTests, test) {
-    EXPECT_TRUE(true);
+TEST (OscillatorTests, test)
+{
+    EXPECT_TRUE (true);
 }
 
-TEST(OscillatorTests, SineSetFrequency_test) {
+TEST (OscillatorTests, SineSetFrequency_test)
+{
     CASPI::BlepOscillator::Sine<float> osc;
-    osc.setFrequency(frequency,sampleRate);
-    EXPECT_GT(osc.phase.increment, -1.0f);
-    EXPECT_NE(osc.phase.increment, 0.0f);
-    EXPECT_LT(osc.phase.increment, 1.0f);
-    EXPECT_NEAR(osc.phase.increment, CASPI::Constants::TWO_PI<float> * phaseIncrement,0.001f);
+    osc.setFrequency (frequency, sampleRate);
+    EXPECT_GT (osc.phase.increment, -1.0f);
+    EXPECT_NE (osc.phase.increment, 0.0f);
+    EXPECT_LT (osc.phase.increment, 1.0f);
+    EXPECT_NEAR (osc.phase.increment, CASPI::Constants::TWO_PI<float> * phaseIncrement, 0.001f);
 }
 
-TEST(OscillatorTests, SawSetFrequency_test) {
+TEST (OscillatorTests, SawSetFrequency_test)
+{
     CASPI::BlepOscillator::Saw<float> osc;
-    osc.setFrequency(frequency,sampleRate);
-    EXPECT_GT(osc.phase.increment, -1.0f);
-    EXPECT_NE(osc.phase.increment, 0.0f);
-    EXPECT_LT(osc.phase.increment, 1.0f);
-    EXPECT_EQ(osc.phase.increment, phaseIncrement);
+    osc.setFrequency (frequency, sampleRate);
+    EXPECT_GT (osc.phase.increment, -1.0f);
+    EXPECT_NE (osc.phase.increment, 0.0f);
+    EXPECT_LT (osc.phase.increment, 1.0f);
+    EXPECT_EQ (osc.phase.increment, phaseIncrement);
 }
 
-TEST(OscillatorTests, SquareSetFrequency_test) {
+TEST (OscillatorTests, SquareSetFrequency_test)
+{
     CASPI::BlepOscillator::Square<float> osc;
-    osc.setFrequency(frequency,sampleRate);
-    EXPECT_GT(osc.phase.increment, -1.0f);
-    EXPECT_NE(osc.phase.increment, 0.0f);
-    EXPECT_LT(osc.phase.increment, 1.0f);
-    EXPECT_EQ(osc.phase.increment, phaseIncrement);
+    osc.setFrequency (frequency, sampleRate);
+    EXPECT_GT (osc.phase.increment, -1.0f);
+    EXPECT_NE (osc.phase.increment, 0.0f);
+    EXPECT_LT (osc.phase.increment, 1.0f);
+    EXPECT_EQ (osc.phase.increment, phaseIncrement);
 }
 
 // Don't need to test triangle as it is based on square
@@ -89,106 +94,108 @@ TEST(OscillatorTests, SquareSetFrequency_test) {
 // Render tests
 
 // test individual sample
-TEST(OscillatorTests, SineGetNextSample_test) {
+TEST (OscillatorTests, SineGetNextSample_test)
+{
     CASPI::BlepOscillator::Sine<float> osc;
-    osc.setFrequency(frequency,sampleRate);
+    osc.setFrequency (frequency, sampleRate);
     const auto s = osc.getNextSample();
-    EXPECT_GE(s, -1.0f);
-    EXPECT_LE(s, 1.0f);
-    EXPECT_NEAR(s, 0.0f,0.01f);
+    EXPECT_GE (s, -1.0f);
+    EXPECT_LE (s, 1.0f);
+    EXPECT_NEAR (s, 0.0f, 0.01f);
 }
 
 // test whole waveform
 /// TODO: replace with better testing strategy: Have pre-generated expected waveforms and try to get them to match
-TEST(OscillatorTests, SineRenderWaveform_test) {
-    const auto internal_pi = CASPI::Constants::TWO_PI<float>;
+TEST (OscillatorTests, SineRenderWaveform_test)
+{
+    const auto internal_pi                    = CASPI::Constants::TWO_PI<float>;
     constexpr auto testInternalPhaseIncrement = internal_pi * phaseIncrement;
     CASPI::BlepOscillator::Sine<float> osc;
-    osc.setFrequency(frequency,sampleRate);
-    EXPECT_NEAR(testInternalPhaseIncrement,osc.phase.increment,0.001f);
+    osc.setFrequency (frequency, sampleRate);
+    EXPECT_NEAR (testInternalPhaseIncrement, osc.phase.increment, 0.001f);
 
-    for (int i = 0;i < samplesToRender; i++) {
+    for (int i = 0; i < samplesToRender; i++)
+    {
         auto s = osc.getNextSample();
-        EXPECT_GE(s, -1.0f);
-        EXPECT_LE(s, 1.0f);
+        EXPECT_GE (s, -1.0f);
+        EXPECT_LE (s, 1.0f);
     }
-
 }
 
-TEST(OscillatorTests, SawRenderWaveform_test) {
+TEST (OscillatorTests, SawRenderWaveform_test)
+{
     const auto internal_pi = CASPI::Constants::TWO_PI<float>;
     CASPI::BlepOscillator::Saw<float> osc;
-    osc.setFrequency(frequency,sampleRate);
+    osc.setFrequency (frequency, sampleRate);
     auto currentPhase = 0.0f;
-    while (currentPhase >= internal_pi) { currentPhase -= internal_pi; }
-    for (int i = 0;i < samplesToRender; i++) {
+    while (currentPhase >= internal_pi)
+    {
+        currentPhase -= internal_pi;
+    }
+    for (int i = 0; i < samplesToRender; i++)
+    {
         auto s = osc.getNextSample();
-        EXPECT_GE(s, -1.0f);
-        EXPECT_LE(s, 1.0f);
+        EXPECT_GE (s, -1.0f);
+        EXPECT_LE (s, 1.0f);
         currentPhase += phaseIncrement;
     }
-
 }
 
-TEST(OscillatorTests, SquareRenderWaveform_test) {
+TEST (OscillatorTests, SquareRenderWaveform_test)
+{
     const auto internal_pi = CASPI::Constants::TWO_PI<float>;
     CASPI::BlepOscillator::Square<float> osc;
-    osc.setFrequency(frequency,sampleRate);
+    osc.setFrequency (frequency, sampleRate);
     auto currentPhase = 0.0f;
-    while (currentPhase >= internal_pi) { currentPhase -= internal_pi; }
-    for (int i = 0;i < samplesToRender; i++) {
+    while (currentPhase >= internal_pi)
+    {
+        currentPhase -= internal_pi;
+    }
+    for (int i = 0; i < samplesToRender; i++)
+    {
         auto s = osc.getNextSample();
-        EXPECT_GE(s, -1.0f);
-        EXPECT_LE(s, 1.0f);
+        EXPECT_GE (s, -1.0f);
+        EXPECT_LE (s, 1.0f);
         currentPhase += phaseIncrement;
     }
-
 }
 
-TEST(OscillatorTests, TriangleRenderWaveform_test) {
+TEST (OscillatorTests, TriangleRenderWaveform_test)
+{
     const auto internal_pi = CASPI::Constants::TWO_PI<float>;
     CASPI::BlepOscillator::Triangle<float> osc;
-    osc.setFrequency(frequency,sampleRate);
+    osc.setFrequency (frequency, sampleRate);
     auto currentPhase = 0.0f;
-    while (currentPhase >= internal_pi) { currentPhase -= internal_pi; }
-    for (int i = 0;i < samplesToRender; i++) {
+    while (currentPhase >= internal_pi)
+    {
+        currentPhase -= internal_pi;
+    }
+    for (int i = 0; i < samplesToRender; i++)
+    {
         auto s = osc.getNextSample();
-        EXPECT_GE(s, -1.0f);
-        EXPECT_LE(s, 1.0f);
+        EXPECT_GE (s, -1.0f);
+        EXPECT_LE (s, 1.0f);
         currentPhase += phaseIncrement;
     }
-
 }
 
-
-TEST(OscillatorTests, renderToBlock_test) {
+TEST (OscillatorTests, renderToBlock_test)
+{
     CASPI::BlepOscillator::Sine<float> osc;
-    osc.setFrequency(frequency,sampleRate);
-    auto test = std::vector<float>(1024);
-    for (int i = 0;i < 1024; i++) {
+    osc.setFrequency (frequency, sampleRate);
+    auto test = std::vector<float> (1024);
+    for (int i = 0; i < 1024; i++)
+    {
         auto s = osc.getNextSample();
         test.push_back (s);
     }
     osc.resetPhase();
     // This API is ugly but will work for now
-    auto output = CASPI::BlepOscillator::renderBlock<CASPI::BlepOscillator::Sine<float>,float>(frequency,sampleRate,1024);
-    for (int i = 0;i < 1024; i++) {
-        EXPECT_EQ(output[i],test[i]);
+    auto output = CASPI::BlepOscillator::renderBlock<CASPI::BlepOscillator::Sine<float>, float> (frequency, sampleRate, 1024);
+    for (int i = 0; i < 1024; i++)
+    {
+        EXPECT_EQ (output[i], test[i]);
     }
 }
-
-//======================================================================================
-// PM tests
-
-TEST(OscillatorTests, PMOperator_test) {
-    CASPI::PMOperator<float> osc;
-    osc.setFrequency(frequency,modIndex,sampleRate);
-    for (int i = 0;i < samplesToRender; i++) {
-        auto s = osc.getNextSample();
-        EXPECT_GE(s, -1.0f);
-        EXPECT_LE(s, 1.0f);
-    }
-}
-
 
 #endif //OSCILLATOR_TEST_H
