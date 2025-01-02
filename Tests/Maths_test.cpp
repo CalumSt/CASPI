c912b6a086d37b57714ad6137f47e1a72a572159
CalumSt <calum.stuart@protonmail.com>
2025-01-02 16:32:50 +0000

    Added Maths test file, added midi-freq conversion

---
+++ Tests/Maths_test.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+#include "Utilities/Caspi_Maths.h"
+
+TEST(MathsTest,MidiToHz_test)
+{
+    const std::vector<int> noteNums = {69, 25, 128};
+    const std::vector<double>  results = {440.0,34.648,13289.75};
+    for (int i = 0; i < noteNums.size(); i++)
+    {
+        auto result = CASPI::Maths::midiNoteToHz<double> (noteNums.at(i));
+        ASSERT_NEAR (result, results.at(i),1e-3);
+    }
+
+}
