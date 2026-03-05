b05d6c08f01bc6ff2e071eb9c817f47534e8db41
CalumSt <calum.stuart@protonmail.com>
2026-03-05 18:29:50 +0000

    Tests/core/Parameter_test.cpp: Add tests for parameter class

---
+++ Tests/core/Parameter_test.cpp
@@ -0,0 +1,86 @@
+// caspi_Parameter_test.cpp
+#include <gtest/gtest.h>
+#include "core/caspi_Parameter.h"
+
+using namespace CASPI::Core;
+
+// ---- Parameter ----
+
+TEST(ParameterTest, DefaultNormalizedIsZero) {
+    Parameter<float> p;
+    EXPECT_FLOAT_EQ(p.getBaseNormalized(), 0.f);
+}
+
+TEST(ParameterTest, SetBaseNormalizedClampsToUnitRange) {
+    Parameter<float> p;
+    p.setBaseNormalized(1.5f);
+    EXPECT_FLOAT_EQ(p.getBaseNormalized(), 1.f);
+    p.setBaseNormalized(-0.5f);
+    EXPECT_FLOAT_EQ(p.getBaseNormalized(), 0.f);
+}
+
+TEST(ParameterTest, LinearScaleMapsBoundaries) {
+    Parameter<float> p(0.f, 100.f, 0.f);
+    p.setBaseNormalized(0.f);
+    p.process(); // converge smoother
+    EXPECT_FLOAT_EQ(p.value(), 0.f);
+    p.setBaseNormalized(1.f);
+    // Run enough iterations for smoother to converge (coeff = 1 by default)
+    p.process();
+    EXPECT_FLOAT_EQ(p.value(), 100.f);
+}
+
+TEST(ParameterTest, LogarithmicScaleMidpointIsGeometricMean) {
+    Parameter<float> p(1.f, 100.f, 0.f);
+    p.setRange(1.f, 100.f, ParameterScale::Logarithmic);
+    p.setBaseNormalized(0.5f);
+    p.process();
+    // Geometric mean of 1 and 100 is 10
+    EXPECT_NEAR(p.value(), 10.f, 0.01f);
+}
+
+TEST(ParameterTest, SmoothingConvergesWithinExpectedTime) {
+    Parameter<float> p;
+    constexpr float sampleRate = 48000.f;
+    constexpr float smoothTime = 0.01f; // 10ms
+    p.setSmoothingTime(smoothTime, sampleRate);
+    p.setBaseNormalized(1.f);
+    // Run 99% convergence: timeSeconds samples
+    int samples = static_cast<int>(smoothTime * sampleRate);
+    for (int i = 0; i < samples; ++i) p.process();
+    EXPECT_GT(p.valueNormalized(), 0.989f);
+}
+
+TEST(ParameterTest, NoSmoothingWithZeroTime) {
+    Parameter<float> p;
+    p.setSmoothingTime(0.f, 48000.f);
+    p.setBaseNormalized(1.f);
+    p.process();
+    EXPECT_FLOAT_EQ(p.valueNormalized(), 1.f);
+}
+
+// ---- ModulatableParameter ----
+
+TEST(ModulatableParameterTest, ClearModulationResetsAccum) {
+    ModulatableParameter<float> p(0.f, 1.f, 0.5f);
+    p.addModulation(0.2f);
+    p.clearModulation();
+    EXPECT_FLOAT_EQ(p.getModulationAmount(), 0.f);
+}
+
+TEST(ModulatableParameterTest, ValueNormalizedClampsWithModulation) {
+    ModulatableParameter<float> p;
+    p.setBaseNormalized(0.9f);
+    // Bypass smoother
+    p.process();
+    p.addModulation(0.5f); // Would push to 1.4
+    EXPECT_FLOAT_EQ(p.valueNormalized(), 1.f);
+}
+
+TEST(ModulatableParameterTest, NegativeModulationClampsToZero) {
+    ModulatableParameter<float> p;
+    p.setBaseNormalized(0.1f);
+    p.process();
+    p.addModulation(-0.5f);
+    EXPECT_FLOAT_EQ(p.valueNormalized(), 0.f);
+}
