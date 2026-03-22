e6d2ad52f6349d5289cc3e38bc4d26e52d7c0a09
CalumSt <calum.stuart@protonmail.com>
2026-03-22 17:01:10 +0000

    caspi.h: add all missing headers

--- CASPI/caspi.h
+++ CASPI/caspi.h
@@ -6,7 +6,9 @@
 #include "base/caspi_Constants.h"
 #include "base/caspi_Features.h"
 #include "base/caspi_Platform.h"
+#include "base/caspi_Compatibility.h"
 #include "base/caspi_Traits.h"
+#include "base/caspi_RealtimeContext.h"
 #include "base/caspi_SIMD.h"
 
 // Core
@@ -15,7 +17,10 @@
 #include "core/caspi_Core.h"
 #include "core/caspi_Expected.h"
 #include "core/caspi_Phase.h"
-// Audio Utils
+#include "core/caspi_Parameter.h"
+
+// External dependencies
+#include "external/caspi_External.h"
 
 // Utilities
 #include "maths/caspi_FFT.h"
@@ -24,6 +29,9 @@
 // oscillators
 #include "oscillators/caspi_BlepOscillator.h"
 #include "oscillators/caspi_Operator.h"
+#include "oscillators/caspi_WaveTableOscillator.h"
+#include "oscillators/caspi_LFO.h"
+#include "oscillators/caspi_Noise.h"
 
 // Filters
 #include "filters/caspi_OnePoleFilter.h"
@@ -33,7 +41,8 @@
 #include "gain/caspi_Gain.h"
 
 // Envelopes
-#include "envelopes/caspi_Envelope.h"
+#include "controls/caspi_Envelope.h"
+#include "controls/caspi_ModMatrix.h"
 
 // Synthesizers
 #include "synthesizers/caspi_FMGraph.h"
