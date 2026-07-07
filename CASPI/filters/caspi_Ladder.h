f21752e7b5b421f36800c9a29c79dbc0d95b930e
CalumSt <calum.stuart@protonmail.com>
2026-07-07 17:31:42 +0100

    caspi_Ladder.h: add stub ladder

---
+++ CASPI/filters/caspi_Ladder.h
@@ -0,0 +1,94 @@
+#ifndef CASPI_LADDER_H
+#define CASPI_LADDER_H
+
+/*************************************************************************
+ *  .d8888b.                             d8b
+ * d88P  Y88b                            Y8P
+ * 888    888
+ * 888         8888b.  .d8888b  88888b.  888
+ * 888            "88b 88K      888 "88b 888
+ * 888    888 .d888888 "Y8888b. 888  888 888
+ * Y88b  d88P 888  888      X88 888 d88P 888
+ *  "Y8888P"  "Y888888  88888P' 88888P"  888
+ *                              888
+ *                              888
+ *                              888
+ *
+ * @file   filters/caspi_Ladder.h
+ * @author CS Islay
+ * @brief  Stub: Filter<FloatType, FilterTopology::Ladder>
+ *
+ * Placeholder for the Moog-style four-pole ladder filter.
+ * Currently acts as an identity pass-through until implemented.
+ *
+ * STATE LAYOUT (NumStates = 4)
+ *
+ *   states[0..3]  z^-1 delay registers for each ladder stage
+ *
+ * COEFFICIENT LAYOUT (NumCoeffs = 6)
+ *
+ *   Reserved for future use.
+ *
+ ************************************************************************/
+
+#include "base/caspi_Features.h"
+#include "filters/caspi_Filter.h"
+
+namespace CASPI
+{
+    namespace Filters
+    {
+
+        template <>
+        struct FilterTraits<FilterTopology::Ladder>
+        {
+            static constexpr std::size_t NumStates = 4;
+            static constexpr std::size_t NumCoeffs = 6;
+        };
+
+        template <CASPI_FLOAT_TYPE FloatType>
+        class Filter<FloatType, FilterTopology::Ladder>
+            : public FilterBase<Filter<FloatType, FilterTopology::Ladder>,
+                                FloatType,
+                                FilterTraits<FilterTopology::Ladder>::NumStates,
+                                FilterTraits<FilterTopology::Ladder>::NumCoeffs>
+        {
+            public:
+                using Base = FilterBase<Filter<FloatType, FilterTopology::Ladder>,
+                                        FloatType,
+                                        FilterTraits<FilterTopology::Ladder>::NumStates,
+                                        FilterTraits<FilterTopology::Ladder>::NumCoeffs>;
+
+                /**
+                 * @brief Default constructor. Initialises sample rate to the
+                 *        project default and zeroes all coefficients.
+                 */
+                Filter() noexcept
+                {
+                    Graph::NodeBase<FloatType>::setSampleRate (Constants::DEFAULT_SAMPLE_RATE<FloatType>);
+                }
+
+                /**
+                 * @brief Compute or recompute all ladder coefficients from the
+                 *        current (cutoff, Q, gain, sampleRate). Stub - no-op.
+                 */
+                void updateCoefficients() noexcept
+                {
+                    // Stub -- no coefficients computed yet.
+                }
+
+                /**
+                 * @brief Process one input sample. Stub - identity pass-through.
+                 * @param x  Input sample.
+                 * @return   Unmodified input sample.
+                 */
+                CASPI_NO_DISCARD FloatType processSample (FloatType x) noexcept CASPI_NON_BLOCKING
+                {
+                    return x;
+                }
+        };
+
+    } // namespace Filters
+} // namespace CASPI
+
+#endif // CASPI_LADDER_H
